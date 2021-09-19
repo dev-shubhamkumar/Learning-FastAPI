09a67d73c8913c9ea0644302a8c454974a933b4c
Shubham Kumar <developer.shubham07@gmail.com>
2021-09-19 17:28:45 +0530

    Learning to use query parameter and Optional Keyword

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    # This is how we implememt query parameters
+    # http://127.0.0.1:8000/blog?limit=1&published=False
+    if published:
+        return {'data': f'blog list has {limit} blogs'}
+    else:
+        return {'data': f'blog list has no blogs'}
 
 
 @app.get('/blog/unpublished')
